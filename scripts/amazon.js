ef0702405e2948d3eb1db51361ade10a783f9aea
achintyasrivastav <rachinka09@gmail.com>
2024-09-04 19:43:06 +0530

    USe modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let productHTML = '';
 
 products.forEach((product) => {
@@ -82,8 +84,15 @@ document.querySelectorAll('.js-add-to-cart')
                 quantity: 1,
             });
         }
-        
-        console.log(cart);
+
+        let cartQuantity = 0;
+        cart.forEach((item) => {
+            cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.cart-quantity')
+         .innerHTML = cartQuantity;
+         
     });
    
 });
